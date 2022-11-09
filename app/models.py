ec17012358b5c1cf139613b7d43804a7291e0641
sumca252 <ismail.ali9315@gmail.com>
2022-11-09 18:58:36 +0100

    adds docstrings

--- app/models.py
+++ app/models.py
@@ -35,18 +35,22 @@ class User(UserMixin, db.Model):
         backref=db.backref('followers', lazy='dynamic'), lazy='dynamic')
 
     def follow(self, user):
+        """ add followers """
         if not self.is_following(user):
             self.followed.append(user)
 
     def unfollow(self, user):
+        """ remove followers """
         if self.is_following(user):
             self.followed.remove(user)
 
     def is_following(self, user):
+        """ check if is following"""
         return self.followed.filter(
             followers.c.followed_id == user.id).count() > 0
 
     def followed_posts(self):
+        """ followed posts """
         followed = Post.query.join(
             followers, (followers.c.followed_id == Post.user_id)).filter(
                 followers.c.follower_id == self.id)
