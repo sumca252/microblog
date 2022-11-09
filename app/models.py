2b2fa67d3ee54969644072e4f0a4fff1bcabe823
sumca252 <ismail.ali9315@gmail.com>
2022-11-09 17:44:07 +0100

    added followers table

--- app/models.py
+++ app/models.py
@@ -9,11 +9,19 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import db, login
 
+followers = db.Table('followers',
+                     db.Column('follower_id', db.Integer,
+                               db.ForeignKey('user.id')),
+                     db.Column('followed_id', db.Integer,
+                               db.ForeignKey('user.id'))
+                     )
+
+
 class User(UserMixin, db.Model):
     """
     Represetns a system User
     """
-    id = db.Column(db.Integer, primary_key=True) 
+    id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
@@ -23,7 +31,7 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f'<User {self.username}, {self.email}>'
-    
+
     def set_password(self, password):
         """
         Set password to generated password hash
@@ -53,6 +61,7 @@ class User(UserMixin, db.Model):
         url = f'https://www.gravatar.com/avatar/{digest}?d=retro&s={size}'
         current_app.logger.debug(f"Get gravatar {url}")
         return url
+
 
 class Post(db.Model):
     """
